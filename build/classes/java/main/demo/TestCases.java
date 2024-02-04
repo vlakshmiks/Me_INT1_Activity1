c30d36657e6ba02be9a41c69dcddf63757fdf223
vlakshmiks <156518357+vlakshmiks@users.noreply.github.com>
2024-02-04 21:45:14 +0530

    Update TestCases.java

--- build/classes/java/main/demo/TestCases.java
+++ build/classes/java/main/demo/TestCases.java
@@ -1,5 +1,8 @@
 package demo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
@@ -50,6 +53,21 @@ public class TestCases {
         System.out.println("end Test case: testCase02");
     }
 
+    //Automate_image_urls
+    public  void testCase03() throws InterruptedException{
+        System.out.println("Start Test case: testCase03");
+        driver.get("https://in.bookmyshow.com/explore/home/chennai");
+        List<WebElement> imageList = new ArrayList<>();
+        imageList = driver.findElements(By.xpath("//*[contains(@class,'style__WidgetContainerBody-sc-lnhrs7-4 YeNog')]/div/a/div/div/div/img"));
+        int countImages = imageList.size();
+        System.out.println("count of images in bookmyshow recommendations is: " + countImages);
+        for (int i=0; i<countImages;i++) {
+             System.out.println("image url is: " + imageList.get(i).getAttribute("src"));
+        }
+        System.out.println("count of images in bookmyshow recommendations is: " + countImages);
+        
+        System.out.println("end Test case: testCase03");
+    }
 
 
 }
